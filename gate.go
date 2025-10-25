8ccdd6a6b564707389d5bb2aaf38079cedd0d467
foxikle <foxikle@cytonic.net>
2025-10-25 13:36:13 -0400

    Support for swapping ports at runtime
    Signed-off-by: foxikle <foxikle@cytonic.net>

--- gate.go
+++ gate.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"strings"
+
 	"github.com/CytonicMC/Cynder/cynder"
 	"go.minekube.com/gate/cmd/gate"
 	"go.minekube.com/gate/pkg/edition/java/proxy"
@@ -15,5 +18,25 @@ func main() {
 	// Gate will take care of everything else for us,
 	// such as config auto-reloading and flags like --debug.
 
+	replaceConfigs()
+
 	gate.Execute()
+}
+
+func replaceConfigs() {
+	data, err := os.ReadFile("config.yml")
+	if err != nil {
+		panic(err)
+	}
+
+	port := os.Getenv("CYNDER_PORT")
+	if port == "" {
+		panic("CYNDER_PORT not set")
+	}
+
+	updated := strings.ReplaceAll(string(data), "${PORT}", port)
+
+	if err := os.WriteFile("config.yml", []byte(updated), 0644); err != nil {
+		panic(err)
+	}
 }
