ef24da0eae98892534dfb1e06f677acdc0e5bf2e
foxikle <foxikle@cytonic.net>
2025-11-05 20:12:13 -0500

    Fix not using prefixed server registrations
    Signed-off-by: foxikle <foxikle@cytonic.net>

--- cynder/natsMsgr/servers/registrations.go
+++ cynder/natsMsgr/servers/registrations.go
@@ -3,17 +3,19 @@ package servers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"time"
+
 	"github.com/CytonicMC/Cydian/servers/registry"
+	"github.com/CytonicMC/Cynder/cynder/env"
 	"github.com/CytonicMC/Cynder/cynder/messaging"
 	"github.com/CytonicMC/Cynder/cynder/util/servers"
 	"github.com/nats-io/nats.go"
 	"go.minekube.com/gate/pkg/edition/java/proxy"
-	"log"
-	"time"
 )
 
 func ListenForServerRegistrations(nc messaging.NatsService, proxy *proxy.Proxy) {
-	const subject = "servers.proxy.startup.notify"
+	subject := env.EnsurePrefixed("servers.proxy.startup.notify")
 
 	err := nc.Subscribe(subject, func(msg *nats.Msg) {
 		var server registry.ServerInfo
@@ -40,7 +42,7 @@ func ListenForServerRegistrations(nc messaging.NatsService, proxy *proxy.Proxy) 
 }
 
 func ListenForServerShutdowns(nc messaging.NatsService, proxy *proxy.Proxy) {
-	const subject = "servers.proxy.shutdown.notify"
+	subject := env.EnsurePrefixed("servers.proxy.shutdown.notify")
 
 	err := nc.Subscribe(subject, func(msg *nats.Msg) {
 		var server registry.ServerInfo
@@ -68,7 +70,7 @@ func ListenForServerShutdowns(nc messaging.NatsService, proxy *proxy.Proxy) {
 }
 
 func FetchServers(nc messaging.NatsService, proxy *proxy.Proxy) {
-	const subject = "servers.proxy.startup"
+	subject := env.EnsurePrefixed("servers.proxy.startup")
 	msg, err := nc.Request(subject, []byte(""), time.Second*30)
 	if err != nil {
 		log.Fatalf("Error fetching servers: %v", err)
